9e127f6fb31444970d1ed7f459e3987c8e6c95f8
a-claus <54470726+a-claus@users.noreply.github.com>
2020-02-13 15:09:17 +0100

    Update readExcel.js

--- readExcel.js
+++ readExcel.js
@@ -1,13 +1,26 @@
 
 
 function click(){
-    console.log("HHH");
     
-      $.get("./testMpl.csv", function(csv, status){
+      $.get("http://karta.denniskarstrom.com/", function(csv, status){
         if(status == "success")
-          alert("External content loaded successfully!" + data);
+          alert("External content loaded successfully!");
+         
+          console.log(csv);
+          
+          const array = csv.split("\n")
+          //.map(function(line){
+            //return line.split("\n")
+         //});
+          console.log(array);
         if(status == "error")
           alert("Error: " + status);
       });
     
 }
+
+
+
+
+
+//let cal = cleanFile(lines);
